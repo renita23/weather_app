000a5e260ced35d7d18a0de570dcc481c113d5e2
Renita23 <r.renitac@gmail.com>
2018-08-29 16:14:26 +0530

    partone 25.46

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "6720005e36f182c86760ebe385db0d9c";
+
+
+class App extends React.Component
+{
+  render()
+  {
+    return(
+    <div> 
+      <Titles />
+      <Form />
+      <Weather /> 
+    </div>
+    );
+  }
+};
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Form extends React.Component
+{
+	render()
+	{
+		return(
+			<div> Form Component </div>
+
+			);
+	}
+};
+ export default Form;
