2d5f370fc5dc3814d69422ab7e6187fe7b68c0c2
samdivyakumar <samdivyakumar@gmail.com>
2018-08-30 07:00:57 +0530

    added API URL

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
-const API_KEY = "6720005e36f182c86760ebe385db0d9c";
+const API_KEY = "6720005e36f182c86760ebe385db0d9c"; //Use your API_KEY : 6720005e36f182c86760ebe385db0d9c
 
-
+//Divya API_KEY: 2e34e22e4fe24c1fbfdcba880786cc04
 class App extends React.Component
 {
-  getWeather = async () =>
+  getWeather = async (e) =>
   {
-    const api_call = await fetch();
+    e.preventDefault();
+    const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Vellore,india&appid=6720005e36f182c86760ebe385db0d9c&units=metric');
+    const data = await api_call.json();
+    console.log(data);
   }
   render()
   {
     return(
     <div> 
       <Titles />
-      <Form />
+      <Form getWeather={this.getWeather}/>
       <Weather /> 
     </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -5,8 +5,11 @@ class Form extends React.Component
 	render()
 	{
 		return(
-			<div> Form Component </div>
-
+			<form onSubmit={this.props.getWeather}>
+			<input type="text" name="city" placeholder="City..."/>
+			<input type="text" name="country" placeholder="Country..."/>
+			<button>Get Weather</button>
+			</form>
 			);
 	}
 };
