975dcaa3199fe95e3cef23b9e60566e0703c5a30
Renita23 <r.renitac@gmail.com>
2018-08-30 15:32:21 +0530

    Added on 30.08.2018

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React from "react";
 import Titles from "./components/Titles";
 import Form from "./components/Form";
-import Weather from "./components/Weather";
+import Weather from "./components/Weather.js";
 
 const API_KEY = "6720005e36f182c86760ebe385db0d9c"; //Use your API_KEY : 6720005e36f182c86760ebe385db0d9c
 
 //Divya API_KEY: 2e34e22e4fe24c1fbfdcba880786cc04
 class App extends React.Component
 {
+  state={
+    temperature : undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
   getWeather = async (e) =>
   {
     e.preventDefault();
-    const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Vellore,india&appid=6720005e36f182c86760ebe385db0d9c&units=metric');
+    
+    const city=e.target.element.city.value;
+    
+    const country=e.target.element.country.value;
+    
+    const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric');
+    
     const data = await api_call.json();
+    
     console.log(data);
+    
+    this.setState({
+      temperature: data.main.temp,
+      city:data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    }); 
   }
   render()
   {
@@ -21,9 +45,16 @@ class App extends React.Component
     <div> 
       <Titles />
       <Form getWeather={this.getWeather}/>
-      <Weather /> 
+      <Weather 
+        temperature = {this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error= {this.state.error}
+      /> 
     </div>
     );
   }
-};
+}; 
 export default App;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,8 +5,13 @@ class Weather extends React.Component
 	render()
 	{
 		return(
-			<div> Weather Component </div>
-
+			<div> 
+			{this.props.city && this.props.country && <p>Location: {this.props.city} , {this.props.country}</p>}
+			{this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+			{this.props.humidity && <p>Humidity : {this.props.humidity}</p>}
+			{this.props.description && <p>Conditions: {this.props.description}</p>}
+			</div>
+ 
 			);
 	}
 };
